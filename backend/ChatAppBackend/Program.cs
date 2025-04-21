21eab85839fc190eef0bf1a404cf96111453c19f
Vladimir Dubecky <dubecky.vd@gmail.com>
2025-04-21 20:41:57 +0200

    feat: added ASP .NET Core project

---
+++ backend/ChatAppBackend/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();

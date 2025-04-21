9e9fe35d0bfec804d075eceed3848f47b6e320f0
Vladimir Dubecky <dubecky.vd@gmail.com>
2025-04-21 21:27:21 +0200

    feat: added DB context

---
+++ backend/ChatAppBackend/ChatAppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ChatAppBackend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatAppBackend
+{
+    public class ChatAppDbContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+
+        public ChatAppDbContext(DbContextOptions options) : base(options) { }
+    }
+}

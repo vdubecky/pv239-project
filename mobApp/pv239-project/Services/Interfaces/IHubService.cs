d0a6f0f8b668c23938cdcc29fdccce9676e0d55f
Vladimir Dubecky <dubecky.vd@gmail.com>
2025-05-14 15:51:59 +0200

    feat(mobApp): added service for SignalR client

---
+++ mobApp/pv239-project/Services/Interfaces/IHubService.cs
@@ -0,0 +1,15 @@
+﻿using pv239_project.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pv239_project.Services.Interfaces
+{
+    public interface IHubService
+    {
+         Dictionary<string, Action<CreateMessageDto>> MessageHandlers { get; set; }
+         Task Start();
+    }
+}

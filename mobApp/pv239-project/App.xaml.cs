be1e00a94cbd4572e012a8991cb7521cd484cb97
Vladimir Dubecky <dubecky.vd@gmail.com>
2025-05-31 17:32:06 +0200

    feat(App): static IsInForeground flag in App

--- mobApp/pv239-project/App.xaml.cs
+++ mobApp/pv239-project/App.xaml.cs
@@ -5,17 +5,41 @@ namespace pv239_project;
 
 public partial class App : Application
 {
-    public AuthViewModel AuthViewModel { get; }
+    public static bool IsInForeground { get; private set; }
     
+    public AuthViewModel AuthViewModel { get; }
+
     public App(AuthViewModel viewModel)
     {
         InitializeComponent();
-        
+
         AuthViewModel = viewModel;
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
         return new Window(new AppShell(AuthViewModel));
+    }
+
+    protected async override void OnStart()
+    {
+        base.OnStart();
+        IsInForeground = true;
+    
+#if ANDROID
+        await Permissions.RequestAsync<NotificationPermission>();
+#endif
+    }
+
+    protected override void OnSleep()
+    {
+        base.OnSleep();
+        IsInForeground = false;
+    }
+
+    protected override void OnResume()
+    {
+        base.OnResume();
+        IsInForeground = true;
     }
 }
